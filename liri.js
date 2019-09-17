d98331096dd4cb20f95de539147488729cbf92ff
Jake Carlson <jakecarlson@Jakes-MBP.zyxel.com>
2019-09-17 13:53:37 -0500

    created all files, liri.js started

---
+++ liri.js
@@ -0,0 +1,9 @@
+// included in instructions, require dotenv to prevent passwords and keys from being displayed
+require("dotenv").config();
+
+// import keys.js
+var keys = require("./keys");
+
+// fs
+var fa = require("fs");
+
