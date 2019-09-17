621f8bb05979c2d68eea4f48d86e345aa7d9b168
Jake Carlson <jakecarlson@Jakes-MBP.zyxel.com>
2019-09-17 14:12:37 -0500

    added dotenv npm, spotify code liri.js

--- liri.js
+++ liri.js
@@ -7,6 +7,19 @@ var keys = require("./keys.js");
 // fs
 var fa = require("fs");
 
+// spotify 
+var Spotify = require('node-spotify-api');
+
 // access spoftify via keys.js
 var spotify = new Spotify(keys.spotify);
 
+
+ 
+ // copied from npmjs.com
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
